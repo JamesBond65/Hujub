630565faaf81ebb4f03b08a78941b0dcfeb1a839
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-08-01 01:20:56 +0200

    avancée

--- Random.1/nouveau.py
+++ Random.1/nouveau.py
@@ -3,23 +3,21 @@ from os import getcwd
 import urllib.request as ur
 
 
-url = 'https://raw.github.com/JamesBond65/Hujub/master/Random.1/dist/update.exe'
-#r = requests.get(url, allow_redirects=True)
 
 directory = getcwd()
 print(directory)
 filename = directory + '\Random.1\ScredAIO.exe'
 print(filename)
-r = requests.get(url, allow_redirects=True)
+
+headers = {
+    'Authorization': 'token 166c3f0372a93dd3c1f9e6358e5b39bfb8b57d76',
+    'Accept': 'application/vnd.github.v3.raw',
+}
+
+r = requests.get('https://raw.githubusercontent.com/AlexisBal/Projet_Bot/master/Bot_Zalando_3.0/Test_telechargement_du_.exe/update.exe', headers=headers, allow_redirects=True)
 
 f = open(filename,'wb')
 f.write(r.content)
-'''
 
+TOKEN = "166c3f0372a93dd3c1f9e6358e5b39bfb8b57d76"
 
-f = ur.urlopen(url)
-file = f.read()
-f.close()
-f2 = open('download.exe', 'wb')
-f2.write(file)
-f2.close()'''
