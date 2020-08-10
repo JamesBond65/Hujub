4f9554390284b9560ccee09f545c4ccd95201553
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-08-10 13:50:28 +0200

    .

--- Random.1/ScredAIO-1.py
+++ Random.1/ScredAIO-1.py
@@ -1,6 +1,21 @@
-if os.path.basename(__file__) == 'ScredAIO-1.exe':
-        
-        directory = os.path.dirname(os.path.realpath(__file__))
-        old_file_name = directory + '\ScredAIO-1.exe'
-        new_file_name = directory +'\ScredAIO.exe'
-        os.rename( old_file_name , new_file_name)
+from urllib.requests import build_opener, HTTPCookieProcessor, HTTPHandler
+import requests
+import http.cookiejar as cookielib
+
+#Create a CookieJar object to hold the cookies
+cj = cookielib.CookieJar()
+#Create an opener to open pages using the http protocol and to process cookies.
+opener = build_opener(HTTPCookieProcessor(cj), HTTPHandler())
+
+#create a request object to be used to get the page.
+req = request.get("https://temp-mail.org/fr/")
+f = opener.open(req)
+
+#see the first few lines of the page
+html = f.read()
+print (html[:50])
+
+#Check out the cookies
+print( "the cookies are: ")
+for cookie in cj:
+    print (cookie)
